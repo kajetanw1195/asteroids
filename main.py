1c100a789c033907c26a81ab9e205a9b0a149e35
kajetanw1195 <kajetan11435@gmail.com>
2025-02-20 19:58:57 +0100

    We got a triangle in the middle

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 import constants
 
+from player import Player 
+
 def main():
-    pygame.init
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        player.draw(screen)
         pygame.display.flip()
-
-
+        dt = clock.tick(60) / 1000
+        
 print("Starting asteroids!")
+
+
 
 
 
@@ -23,4 +28,7 @@ if __name__ == "__main__":
     main() 
 
 
+
+
+    # source venv/bin/activate --> to activate virtual machine for pygame
 
